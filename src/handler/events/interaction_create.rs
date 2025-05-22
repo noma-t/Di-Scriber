8357a9d22fb0f24140b603af54a89adce98ca59d
AomiVel <kaitoyoutube@icloud.com>
2025-05-22 23:28:29 +0900

    Add modal handling

--- src/handler/events/interaction_create.rs
+++ src/handler/events/interaction_create.rs
@@ -5,16 +5,18 @@ use serenity::all::{
 
 use crate::handler::interactions::{
     commands::handle_commands,
-    components::handle_components
+    components::handle_components,
+    modals::handle_modals
 };
 
 pub async fn interaction_create(ctx: &Context, interaction: Interaction) {
-    let config = crate::config::get();
+    // let config = crate::config::get();
     
     match interaction {
         // Slash commands
         Interaction::Command(command) => handle_commands(&ctx, &command).await,
         Interaction::Component(component) => handle_components(&ctx, &component).await,
+        Interaction::Modal(component) => handle_modals(&ctx, &component).await,
         _ => { println!("interaction not found"); }
     }
 }
