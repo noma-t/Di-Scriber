77e20fdecdbbbf93ebda7b047eebc90f589286f1
AomiVel <kaitoyoutube@icloud.com>
2025-05-24 14:46:15 +0900

    fix: unused intents

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod handler;
 mod config;
 
 use serenity::{Client};
+use serenity::prelude::{GatewayIntents};
 use handler::handler::Handler;
 
 
@@ -9,11 +10,13 @@ use handler::handler::Handler;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     config::init()?;
 
+    let intents = GatewayIntents::default();
+
     let mut client = Client::builder(&config::get().discord.bot_token, intents)
         .event_handler(Handler)
         .await?;
-    
+
     client.start().await?;
-    
+
     Ok(())
 }
